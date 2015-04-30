8861e3e55bcd65be641e828effc55046cc021011
pabloem <polecito.em@gmail.com>
2015-04-30 20:56:15 +0900

    Added the 'root' query type. It queries from the dictionary - in this case, a dictionary of chinese characters.

--- bin/turtle.js
+++ bin/turtle.js
@@ -1,13 +1,27 @@
+#!/usr/bin/env node
 var HanjaServer = require('../lib/HanjaServer.js'),
     sys = require('sys'),
     serv = require('http'),
-    express = require('express');
+    express = require('express'),
+    fs = require('fs');
 
 var app = express();
 
-var hs = new HanjaServer({'graphFile':'../data/kor_graph.graphml'});
+var args = process.argv.slice(2);
+if(args.length < 1 || args.length > 3) {
+    console.log("Usage: ./turtle.js config.json");
+    return process.exit(1);
+}
+var configFile = args[0],
+    config = JSON.parse(fs.readFileSync(configFile));
 
-var __dirname = "/home/pablo/codes/turtle/site";
+console.log("Read new configuration: ");
+console.log(config);
+
+var hs = new HanjaServer({ 'graphFile' : config.graphFile,
+                           'dictFile' : config.dictFile});
+
+var __dirname = config.siteDir;
 app.get('/runquery', function(req,res) { hs.handleQuery(req,res); });
 app.get('/demoquery', function(req,res) { req.headers.query = 'search as'; hs.handleQuery(req,res); });
 app.use(express.static(__dirname));
