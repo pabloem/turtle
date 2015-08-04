2224ec43ee6936af58e4e631ed66df9107538d0d
Pablo <polecito.em@gmail.com>
2015-08-04 17:19:39 +0200

    Working version with new layout

---
+++ new_site/javascript/HanjaExplorer.js
@@ -0,0 +1,198 @@
+var HanjaExplorer = function(hanjaRequester, config) {
+    this._hr = hanjaRequester;
+    this._r = new sigma({container: 'container',
+                         settings: {
+                             doubleClickEnabled: false
+                         }
+                        });
+    this._neighbor_scale = [7,10,13,16,19,25,33,45,60,80,110,160,200,5000];
+    this.refresh();
+
+    var he = this;
+    this._r.bind('clickNode', function(e) {
+        var node = e.data.node;
+        he._showNodeDetails(node);
+    });
+    this._r.bind('doubleClickNode',function(e) {
+        var node_id = e.data.node.id;
+        he._setFocus(node_id);
+    });
+};
+HanjaExplorer.prototype._displayRootInfo = function() {
+    if(!(this._hr.req.readyState == 4 && this._hr.req.status == 200)) return;
+    var res = eval(this._hr.req.responseText);
+    if(res.length == 0) return;
+    res = res[0];
+    var det = {};
+    det.main = res.chinese;
+    if(res.radicals) {
+        det.secondary = "Radicals: <p>"+res.radicals+"</p>";
+    } else if (res.english) {
+        det.secondary = res.english;
+    }
+    det.tertiary = res.meaning;
+    fillInDetails(det);
+    this._hr.searchRequest(res.chinese,searchQueryResult,false);
+};
+HanjaExplorer.prototype.displayRoot = function(root) {
+    this._hr.rootRequest(root,this._displayRootInfo.bind(this));
+};
+HanjaExplorer.prototype._showNodeDetails = function(node) {
+    var det = {};
+    det.main = node.chinese.split("");
+    det.secondary = node.korean;
+    det.tertiary = node.english;
+    fillInDetails(det);
+    setRootsClickEvent();
+};
+HanjaExplorer.prototype.showNode = function(node,neighbors) {
+    if("undefined" == typeof neighbors) neighbors = true;
+    this._addNode(node);
+    this._showNodeDetails(node);
+    this._setFocus(node.id, neighbors);
+};
+
+HanjaExplorer.prototype._addExtraEdges = function(node){
+    console.log("Adding edges for nodes already in the graph.");
+    var ngs = this._r.graph.nodes(node.neighbors);
+    for(var i=0; i<ngs.length; i++) {
+        if("undefined" == typeof ngs[i]) continue;
+        var from = node.id < ngs[i].id ? node.id : ngs[i].id;
+        var to = node.id >= ngs[i].id ? node.id : ngs[i].id;
+        this.addEdge(from, to);
+    }
+};
+
+// Ready
+HanjaExplorer.prototype._addNode = function(node) {
+    var gr = this._r.graph.nodes(node['id']);
+    if (gr) {
+        // The node is already in the graph
+        return ;
+    }
+    // The nodes don't come with SigmaJS properties added, so we add:
+    node.x = Math.random();
+    node.y = Math.random();
+    node.color = '#AAAAAA';
+    node.size = 1;
+    node.label = 
+        node.korean+" - "+node.chinese+"\n"+
+        node.english;
+    this._r.graph.addNode(node);
+    if(node.neighbors) {
+        this._addExtraEdges(node);
+    }
+    this.refresh();
+};
+
+// Ready
+HanjaExplorer.prototype._configForceAtlasTimeout = function(sig, timeout) {
+    console.log("Setting layout algorithm timeout: "+timeout);
+    setTimeout(function() { 
+        console.log("Stopping force atlas layout algorithm");
+        sig.killForceAtlas2();},timeout);
+    console.log("Set up timeout for ForceAtlas layout");
+};
+
+// Ready
+HanjaExplorer.prototype.fixLayout = function(timeout) {
+    if(!timeout) {
+        timeout = 500;
+    }
+    if(this._r.isForceAtlas2Running()) {
+        console.log("ForceAtlas algorithm is running at the moment.");
+        return;
+    }
+    this._r.startForceAtlas2({worker: false, barnesHutOptimize: false,
+                              iterationsPerRender: 1000});
+    this._configForceAtlasTimeout(this._r,timeout);
+};
+
+// Not Ready
+HanjaExplorer.prototype._dropNeighbors = function (node_id) {
+    console.log("Dropping neighbors of " + node_id + " - not fully implemented.");
+    var nd = this._r.graph.nodes(node_id);
+    var nodes_to_remove = nd.neighbors;
+    for(var i = 0; i < nodes_to_remove.length; i++) {
+        if (nodes_to_remove[i] == this._focus) continue;
+        if(!this._r.graph.nodes(nodes_to_remove[i])) continue;
+        this._r.graph.dropNode(nodes_to_remove[i]);
+    }
+    this.refresh();
+};
+
+HanjaExplorer.prototype._setFocus = function(node_id, neighbors) {
+    if("undefined" == typeof neighbors) neighbors = true;
+    var old_focus = this._focus;
+    this._focus = node_id;
+//    if (old_focus) this._dropNeighbors(bye_focus);
+    this._focus = node_id;
+    if(neighbors) {
+        this._addNeighbors(node_id);
+    }
+};
+
+// Ready
+HanjaExplorer.prototype.addEdge = function(from,to) {
+    if (this._r.graph.edges(from+" "+to) || 
+        this._r.graph.edges(to+" "+from)) {
+        console.log("Edge " + from + " " + to + 
+                    " is already in the graph");
+        return ;
+    }
+    console.log("Adding edge: "+from+" "+to);
+    var edge = {id: from+" "+to,
+                source: from, 
+                target: to};
+    this._r.graph.addEdge(edge);
+    this.refresh();
+};
+
+HanjaExplorer.prototype._receiveAddNeighbors = function(node_id) {
+    if(!(this._hr.req.readyState == 4 && this._hr.req.status == 200))
+        return;
+    var res = eval(this._hr.req.responseText);
+    for(var i = 0; i < res.length; i++) {
+        this._addNode(res[i]);
+    }
+    for(var j = 0; j < res.length; j++) {
+        var from = node_id < res[j].id ? node_id : res[j].id;
+        var to =  node_id >= res[j].id ? node_id : res[j].id;
+        this.addEdge(from,to);
+    }
+    this.fixLayout();
+};
+
+HanjaExplorer.prototype._addNeighbors = function(node_id) {
+    var nd = this._r.graph.nodes(node_id);
+    if(!nd.neighbors) {
+        console.log("Node has no neighbors. Can't add them.");
+        return ;
+    }
+    var _this = this;
+    if("undefined" == typeof nd.neighbor_scale) {
+        nd.neighbor_scale = 0;
+    }
+    var nb_list = 
+            nd.neighbors.slice(0,
+                               this._neighbor_scale[
+                                   nd.neighbor_scale]);
+    if(this._neighbor_scale[nd.neighbor_scale] > nd.neighbors.length) {
+        return;
+    }
+    nd.neighbor_scale += 1;
+    this._hr.nodeRequest(nb_list, 
+                         function() {
+                         _this._receiveAddNeighbors.bind(_this)(node_id);
+                         });
+    return ;
+};
+
+HanjaExplorer.prototype.clear = function() {
+    this._r.graph.clear();
+    this.refresh();
+};
+
+HanjaExplorer.prototype.refresh = function() {
+    this._r.refresh();
+};
---
+++ new_site/javascript/main.js
@@ -0,0 +1,67 @@
+var hr = new HanjaReqs();
+var he = new HanjaExplorer(hr);
+/*$(function() {
+    $("#dialog").dialog({width: 600,
+                         modal: true});
+});*/
+
+$(function(){ 
+    var t = null;
+    $("#q").keyup(function(){ 
+        if (t) { 
+            clearTimeout(t); 
+        } 
+        t = setTimeout("livesearch()", 400); 
+    }); 
+});
+
+function fillInDetails(node) {
+    var hanjas = node.main,
+        res = "";
+    for(var i = 0; i < hanjas.length; i++) {
+        res += "<span class=\"infobox-chinese-char\">"+hanjas[i]+"</span>";
+    }
+    $("#det_main").html(res);
+    $("#det_secondary").html(node.secondary);
+    $("#det_tertiary").html(node.tertiary);
+}
+function setRootsClickEvent() {
+    $("#det_main span").on('click', function() {
+        var root = $(this).text();
+        $("#q").val(root);
+        he.displayRoot(root);
+    });
+}
+
+function searchQueryResult() {
+    console.log(hr.req.status);
+    if(hr.req.readyState == 4 && hr.req.status == 200) {
+        var res = eval(hr.req.responseText);
+        $("#results").html("");
+        var i = 0;
+        $.each(res, 
+               function() {
+                   var cont =
+                           "<div class=\"word-list-item pure-g\">"+
+                           "<div class=\"pure-u-1-2 word-list-chinese\">"+this.chinese+"</div>"+
+                           "<div class=\"pure-u-1-2 word-list-korean\">"+this.korean+"</div>"+
+                           "<div class=\"pure-u-1 word-list-english\">"+this.english+"</div>"+
+                           "</div>";
+                   $("#results").append($(cont).data(this));
+               });
+        $('.word-list-item').on('click',function(){
+            var nod = $(this).data();
+            he.showNode($(this).data(),false);
+        });
+    }
+};
+
+/* Works! TODO - remove comment*/
+function livesearch(){
+    var q = $("#q").val().toLowerCase(); 
+    if (q.length == 0) { 
+        //$("#results").html("");
+    } else { 
+        hr.searchRequest(q,searchQueryResult);
+    }
+};
