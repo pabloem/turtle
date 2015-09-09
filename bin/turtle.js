941f1414af45c44a1ce3164fdf33ad44aa881945
Pablo <polecito.em@gmail.com>
2015-09-09 17:13:21 +0900

    Feedback feature works!

--- bin/turtle.js
+++ bin/turtle.js
@@ -37,7 +37,17 @@ if(cluster.isMaster) {
     var __dirname = config.siteDir;
   app.get('/runquery', function(req,res) { hs.handleQuery(req,res); });
   app.get('/demoquery', function(req,res) { req.headers.query = 'search as'; hs.handleQuery(req,res); });
-  app.get('/feedback', function(req,res) { res.writeHeader(200,{"Content-Type": "text/html; charset=utf-8"});
+  app.get('/feedback', function(req,res) { if(req.query.name == "") return;
+                                           var chunk = "Name: "+req.query.name+"\n"+
+                                               "Email: "+req.query.email+"\n"+
+                                               "Comment: {"+req.query.comment+"}\n\n";
+                                           fs.stat("feedback.txt",function(err,stat) {
+                                             if(err || stat.size < 1000000000) { // If the file doesn't exist or is less than 1GB
+                                               fs.writeFile("feedback.txt",chunk,function(err){});
+                                             }
+                                           });
+                                           // req.query is a dictionary with the parsed variables!
+                                           res.writeHeader(200,{"Content-Type": "text/html; charset=utf-8"});
                                            res.end(); });
     app.get('/facebook', function(req,res) { console.log("IP of bad guy: "); console.log(req.ip);
                                              res.writeHeader(200,{"Content-Type": "text/html; charset=utf-8"});
